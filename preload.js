ec48966bdfbd693fb291b5cac1f2b60b4a6cfcb2
mylxiaoyi <mylxiaoyi@gmail.com>
2022-03-07 18:34:52 +0800

    modify preload.js for ipc

--- preload.js
+++ preload.js
@@ -1,3 +1,20 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+const validChannels = ['SEND_MESSAGE']
+contextBridge.exposeInMainWorld(
+    'ipc', {
+        send: (channel, data) => {
+            if (validChannels.includes(channel)) {
+                ipcRenderer.send(channel, data)
+            }
+        },
+        on: (channel, func) => {
+            if (validChannels.includes(channel)) {
+                ipcRenderer.on(channel, (event, ...args) => func(...args))
+            }
+        }
+    }
+)
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
         const element = document.getElementById(selector)
